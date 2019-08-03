163aef145eca13b540413e2cd57dc69021e0526c
Boris <kaban1123581321@gmail.com>
2019-08-03 14:23:36 +0200

    corect HomeWork1

--- HomeWork1/src/com/edu/HomeTusk.java
+++ HomeWork1/src/com/edu/HomeTusk.java
@@ -38,11 +38,24 @@ class HomeTusk3 {
         int c2 = 8;
         float c3 = 2.5f;
 
-        System.out.println("Where you coll?");
+        System.out.println("Where you colled?");
         String country1 = br.readLine();
         System.out.println("How long did you tolk?");
         int cost1 = Integer.parseInt(br.readLine());
+        System.out.println("You did to " + country1 + " you coll is cost " + c1*cost1 );
 
+        System.out.println("Where you colled?");
+        String country2 = br.readLine();
+        System.out.println("How long did you tolk?");
+        int cost2 = Integer.parseInt(br.readLine());
+        System.out.println("You did to " + country2 + " you coll is cost " + c2*cost2 );
+
+
+        System.out.println("Where you colled?");
+        String country3 = br.readLine();
+        System.out.println("How long did you tolk?");
+        int cost3 = Integer.parseInt(br.readLine());
+        System.out.println("You did to " + country3 + " you coll is cost " + c3*cost3 );
 
 
 
