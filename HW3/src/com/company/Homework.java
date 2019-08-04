6044abb116f007f8d4f2f4bb45928e9bae9f17c0
Denys Yankiv <denys.yankiv@gmail.com>
2019-08-04 19:46:46 +0300

    Added HM3

---
+++ HW3/src/com/company/Homework.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Homework {
+    public static void main(String[] args) throws IOException {
+        Person p1 = new Person();
+        Person p2 = new Person();
+        Person p3 = new Person();
+        Person p4 = new Person();
+        Person p5 = new Person();
+
+        p1.input();
+        p2.input();
+        p3.input();
+        p4.input();
+        p5.input();
+
+        p1.output();
+        p2.output();
+        p3.output();
+        p4.output();
+        p5.output();
+
+
+    }
+}
---
+++ HW3/src/com/company/Person.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.Year;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        if (birthYear == 0) {
+            return 0;
+        }
+        return Year.now().getValue() - birthYear;
+    }
+
+    public void input() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Person first name = ");
+        this.firstName = bf.readLine();
+
+        System.out.print("Person last name = ");
+        this.lastName = bf.readLine();
+
+        System.out.print("Person birthday year = ");
+        this.birthYear = Integer.parseInt(bf.readLine());
+
+    }
+
+    public void output() {
+        System.out.println("Person [ firstName = " + firstName +
+                ", lastName = " + lastName + ", age = " + getAge() + "]");
+    }
+
+    public void changeName(String fn, String ln) {
+        this.firstName = fn;
+        this.lastName = ln;
+    }
+}
