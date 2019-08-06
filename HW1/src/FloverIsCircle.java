3f38df4e3ea5027ef6f13d9b09238d5a503955a2
Your Name <your.email@example.com>
2019-08-06 14:09:35 +0300

    FloverIsCircle

---
+++ HW1/src/FloverIsCircle.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FloverIsCircle {
+	
+
+	public static void main(String[] args) throws NumberFormatException, IOException {		
+		double area;
+		double radius;
+		double perimeter;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("enter the radius of the circle:");
+		radius = Double.parseDouble(reader.readLine());
+		area = Math.PI * Math.pow(radius, 2);
+		perimeter = 2*Math.PI*radius;
+		System.out.println("the area of the circle is equal " +area);
+		System.out.println("the perimeter of the circle is equal " +perimeter);
+			
+
+	}
+
+}
